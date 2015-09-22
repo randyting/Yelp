1f1c58fa5fc60a2baad2dd2e18bdc8f40806fb26
Randy Ting <randy@randyting.com>
2015-09-22 01:11:53 -0700

    Started project with file structure, installed Cocoapods, added a couple pods to Cocoapods.

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.4'
+# inhibit_all_warnings!
+use_frameworks!
+
+target 'Yelp' do
+
+pod "AFNetworking"
+pod "BDBOAuth1Manager"
+
+end
+
---
+++ Yelp/Source/ViewControllers/BusinessesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BusinessesViewController.swift
+//  Yelp
+//
+//  Created by Randy Ting on 9/22/15.
+//  Copyright © 2015 Randy Ting. All rights reserved.
+//
+
+import UIKit
+
+class BusinessesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
