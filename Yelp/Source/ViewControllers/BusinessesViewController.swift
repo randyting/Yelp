70f548a8466b8567734df3ecc7a11208343b83b2
Randy Ting <randy@randyting.com>
2015-09-22 02:53:20 -0700

    Started setting up table view.  Added search bar and filter button.

--- Yelp/Source/ViewControllers/BusinessesViewController.swift
+++ Yelp/Source/ViewControllers/BusinessesViewController.swift
@@ -8,37 +8,64 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController{
   
+  @IBOutlet weak var businessesTableView: UITableView!
   var businesses: [Business]!
+  let searchBar = UISearchBar()
+  
+  // MARK: - Lifecycle
+  
+  override func loadView() {
+    super.loadView()
+    
+    setupSearchBar(searchBar)
+    setupNavigationItem(navigationItem)
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    //        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
-    //            self.businesses = businesses
-    //
-    //            for business in businesses {
-    //                println(business.name!)
-    //                println(business.address!)
-    //            }
-    //        })
-    
-    Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
-      self.businesses = businesses
-      
-      for business in businesses {
-        print(business.name!)
-        print(business.address!)
-      }
-    }
+    setupTableView(businessesTableView)
+    searchForBusinesses()
   }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
   
+  // MARK: - Initialization
+  
+  func setupTableView(tableView: UITableView!) {
+    tableView.delegate = self
+    tableView.dataSource = self
+    tableView.estimatedRowHeight = 100
+    tableView.rowHeight = UITableViewAutomaticDimension
+    
+  }
+  
+  func setupSearchBar(searchBar: UISearchBar) {
+    navigationItem.titleView = searchBar
+  }
+  
+  func setupNavigationItem(navigationItem: UINavigationItem) {
+    self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(title: "Filter", style: .Plain, target: self, action: "filterButtonClicked:")
+  }
+  
+  // MARK: - Behavior
+  
+  func filterButtonClicked(sender: BusinessesViewController) {
+    print("Clicked filter button")
+  }
+  
+  func searchForBusinesses() {
+    Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
+      self.businesses = businesses
+      dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        self.businessesTableView.reloadData()
+      })
+    })
+  }
   /*
   // MARK: - Navigation
   
@@ -48,5 +75,30 @@ class BusinessesViewController: UIViewController {
   // Pass the selected object to the new view controller.
   }
   */
+  
+}
+
+extension BusinessesViewController:  UITableViewDelegate, UITableViewDataSource {
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("BusinessCell", forIndexPath: indexPath)
+    
+    let business = businesses[indexPath.row]
+    
+    cell.textLabel!.text = business.name!
+    
+    return cell
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    if let businesses = businesses {
+      return businesses.count
+    }
+    return 0
+  }
+  
+}
+
+extension BusinessesViewController: UISearchBarDelegate {
   
 }
